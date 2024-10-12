40e1815c52ac2e3f82ce482f1f4217d962271e34
jimmy-moringa <jimmy.musyoki@student.moringaschool.com>
2024-10-12 08:32:01 +0300

    added routes.py

---
+++ server/routes.py
@@ -0,0 +1,13 @@
+from flask import jsonify, request
+from app import app, db
+from models import Hero, Power, HeroPower
+
+@app.route('/heroes', methods=['GET'])
+def get_heroes():
+    heroes = Hero.query.all()
+    return jsonify([{"id": hero.id, "name": hero.name, "super_name": hero.super_name} for hero in heroes])
+
+@app.route('/powers', methods=['GET'])
+def get_powers():
+    powers = Power.query.all()
+    return jsonify([{"id": power.id, "name": power.name, "description": power.description} for power in powers])
